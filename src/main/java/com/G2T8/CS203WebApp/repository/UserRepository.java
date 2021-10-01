c770489fec53ff06739c9bbf12b387d2737ab21f
Jenpoer <jeistory1@gmail.com>
2021-10-01 23:04:49 +0800

    Add JWT authentication filter, generator, and configuration. Make changes to User entity and service.

--- src/main/java/com/G2T8/CS203WebApp/repository/UserRepository.java
+++ src/main/java/com/G2T8/CS203WebApp/repository/UserRepository.java
@@ -3,11 +3,16 @@ package com.G2T8.CS203WebApp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.G2T8.CS203WebApp.model.*;  
+import com.G2T8.CS203WebApp.model.*;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * used to find a user using their email
+     * 
+     * @param email is the email of the user
+     */
+    public User findByEmail(String email);
 
-    
 }
--- src/main/java/com/G2T8/CS203WebApp/service/UserService.java
+++ src/main/java/com/G2T8/CS203WebApp/service/UserService.java
@@ -6,26 +6,34 @@ import com.G2T8.CS203WebApp.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
 
-    
-    public List<User> getAllUsers(){
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    public User getUser(Long ID){
+    public User getUser(Long ID) {
 
         return userRepository.findById(ID).map(user -> {
             return user;
         }).orElse(null);
-        
+
     }
 
-    public User updateUserEmail(Long ID, String email){
+    public User updateUserEmail(Long ID, String email) {
         Optional<User> b = userRepository.findById(ID);
         if (b.isPresent()) {
             User user = b.get();
@@ -59,13 +67,13 @@ public class UserService {
     }
 
     // public User updateUserTeamID(Long ID, Long TeamID) {
-    //     Optional<User> b = userRepository.findById(ID);
-    //     if (b.isPresent()) {
-    //         User user = b.get();
-    //         user.setteam(TeamID);
-    //         return userRepository.save(user);
-    //     } else
-    //         return null;
+    // Optional<User> b = userRepository.findById(ID);
+    // if (b.isPresent()) {
+    // User user = b.get();
+    // user.setteam(TeamID);
+    // return userRepository.save(user);
+    // } else
+    // return null;
 
     // }
 
@@ -91,5 +99,23 @@ public class UserService {
 
     }
 
-    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // In our case, username will be email
+        User user = userRepository.findByEmail(email);
+
+        // If there is no user with that email, throw UsernameNotFoundException
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+
+        // Add authorization -- currently only takes in one role as per the User entity
+        // specifications
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole()));
+
+        // Return the Spring Framework User object, not our custom one!
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+
 }
