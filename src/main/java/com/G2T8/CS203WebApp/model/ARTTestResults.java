4224650f823cf2e1d1ceb0f8862cb2c967a9149d
StanfordDC <stanforddchandra@gmail.com>
2021-10-05 15:07:53 +0800

    Create ARTTestResult model

--- src/main/java/com/G2T8/CS203WebApp/model/ARTTestResults.java
+++ src/main/java/com/G2T8/CS203WebApp/model/ARTTestResults.java
@@ -1,7 +1,19 @@
 package com.G2T8.CS203WebApp.model;
 
-public class ARTTestResults {
+import java.io.Serializable;
+import javax.persistence.*;
+import lombok.*;
 
-    
+@Entity
+public class ARTTestResults {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long artId;
+    private LocalDateTime weeksMonday;
+    private Boolean artResult;
+    private LocalDateTime date;
+    @ManyToOne
+    @JoinColumn(name='user_id')
+    private User user;
     
 }
