624a65e0596e2e268ae2dbfcbd6de1a1501c4b76
StanfordDC <stanforddchandra@gmail.com>
2021-10-06 14:23:27 +0800

    Update ARTTestResult repo

--- src/main/java/com/G2T8/CS203WebApp/repository/ARTTestResultRepository.java
+++ src/main/java/com/G2T8/CS203WebApp/repository/ARTTestResultRepository.java
@@ -6,4 +6,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ARTTestResultRepository extends JpaRepository<ARTTestResult, Long>{
     
+    @Query(value = "SELECT * FROM arttestresult a WHERE a.user_id = user_id", nativeQuery= true)
+    List<Optional<ARTTestResult>> findByUserId(@Param("user_id") Long user_id);
+
+    @Query(value = "SELECT * FROM arttestresut a WHERE a.user_id = user_id and a.date =  date", nativeQuery = true)
+    Optional<ARTTestResult> findByUserIdAndDate(@Param("user_id") Long user_id, 
+            @Param("date") LocalDateTime date);
+}
 }
