4f770e050645da576394a7a71a0cf8bce591b97e
Jenpoer <80697874+Jenpoer@users.noreply.github.com>
2021-10-06 17:57:33 +0800

    [Feature/authentication] JWT Authentication (#1)
    * Add JWT authentication filter, generator, and configuration. Make changes to User entity and service.
    
    * Add JWT secret
    
    * Added user controller & user DTO & JWT req/res
    
    * Reconfigured constraints on User entity & authorization
    
    * Added Cross Origin support
    
    * minor edit to login & register, untrack dsstore
    
    * Added integration testing for auth
    
    * Refactor UserController into JwtAuthController
    
    * Bugfix - changed wrong package name
    
    * Bugfix -- fix file names
    
    * Bugfix (2) -- rename files
    
    * Bugfix (3) - change file names
    
    * Bugfix - rename UserNotFoundException
    
    * Bugfix -- initialize firstLogin when user added
    
    Co-authored-by: claire <claire@eightfour.sg>

--- src/main/java/com/G2T8/CS203WebApp/model/User.java
+++ src/main/java/com/G2T8/CS203WebApp/model/User.java
@@ -1,68 +1,74 @@
 package com.G2T8.CS203WebApp.model;
+
 import java.util.*;
 
 import javax.persistence.*;
-import lombok.*; 
+import lombok.*;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Data // A shortcut for @ToString, @EqualsAndHashCode, @Getter on all fields, and
       // @Setter on all non-final fields, and @RequiredArgsConstructor(generate
       // constructor with args annotated with @NonNull)
+@NoArgsConstructor
 @Entity
 @Table(name = "User")
-public class User implements Serializable{
+public class User implements Serializable {
 
-    private static final long serialVersionUID = 1L; 
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long ID; 
+    private Long ID;
 
-    @Column(name = "name", nullable = false) @NonNull
-    private String name; 
+    @Column(name = "name", nullable = false)
+    @NotEmpty
+    private String name;
 
-
-    @Column(name = "email", nullable = false, unique = true, length = 45) @NotNull @Email
+    @Column(name = "email", nullable = false, unique = true, length = 45)
+    @NotEmpty
+    @Email
     private String email;
 
-    @Column(name = "vaccinationStatus", nullable = false)
-    @NonNull
+    @Column(name = "vaccinationStatus") // , nullable = false)
+    // @NonNull
     private int vaccinationStatus;
 
     @Column(name = "role", nullable = false)
-    @NonNull
+    @NotEmpty
     private String role;
 
     @Column(name = "password", nullable = false)
-    @Size(min = 8, max = 30)
+    // @Size(min = 8, max = 30)
+    @NotEmpty
     private String password;
 
-    // Foreign key of Team class to identify which team the user is in; 
+    // Foreign key of Team class to identify which team the user is in;
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "teamID")
-    @NonNull
+    // @NonNull
     private Team team;
 
-    // Recursive key of User class to identify which user is managing a particular user object;
+    // Recursive key of User class to identify which user is managing a particular
+    // user object;
     @OneToMany(mappedBy = "ManagerUser", orphanRemoval = true, cascade = CascadeType.ALL)
-    private Set<User> EmployeeUsers; 
+    private Set<User> EmployeeUsers;
 
     @ManyToOne
     @JoinColumn(name = "ManagerUser_Id")
     private User ManagerUser;
 
     // user id becomes a foreign key for class/table schedule
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL , orphanRemoval = true)
-    private List<Schedule> schedules; 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Schedule> schedules;
 
     // user id becomes a foreign key for CovidHistory class/table
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CovidHistory> covidHistories;
 
-    // user id becomes a foreign key for Officerequest 
+    // user id becomes a foreign key for Officerequest
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OfficeRequest> officeRequests;
 
@@ -76,16 +82,3 @@ public class User implements Serializable{
 
 
 }
-
-
-
-
-    
-
-
-    
-
-
-
-    
-
---
+++ src/test/java/com/G2T8/CS203WebApp/JWTAuthIntegrationTest.java
@@ -0,0 +1,180 @@
+package com.G2T8.CS203WebApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.URI;
+import java.util.Optional;
+
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import io.restassured.RestAssured;
+import io.restassured.config.JsonConfig;
+import io.restassured.path.json.config.JsonPathConfig;
+import io.restassured.response.Response;
+import net.minidev.json.JSONObject;
+import static io.restassured.config.RedirectConfig.redirectConfig;
+import static org.hamcrest.Matchers.equalTo;
+
+import static io.restassured.RestAssured.*;
+
+import com.G2T8.CS203WebApp.repository.UserRepository;
+import com.G2T8.CS203WebApp.model.User;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class JWTAuthIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private final String baseUrl = "http://localhost:";
+    private final String baseEndpoint = "/api/v1/users";
+
+    @Autowired
+    private UserRepository users;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @BeforeAll
+    public static void initClass() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.useRelaxedHTTPSValidation();
+        RestAssured.urlEncodingEnabled = false;
+        RestAssured.config = RestAssured.config()
+                .jsonConfig(JsonConfig.jsonConfig().numberReturnType(JsonPathConfig.NumberReturnType.DOUBLE))
+                .redirect(redirectConfig().followRedirects(false));
+    }
+
+    /**
+     * Clear database after every test
+     */
+    @AfterEach
+    void tearDown() {
+        users.deleteAll();
+    }
+
+    // --------------------------------------------------
+    // Tests for saving user into database
+    // --------------------------------------------------
+
+    @Test
+    public void saveUser_Success() throws Exception {
+        URI uri = new URI(baseUrl + port + baseEndpoint + "/register");
+
+        // Create request body in JSON
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", "test420@gmail.com");
+        requestBody.put("name", "Test");
+        requestBody.put("password", "whatpassword");
+        requestBody.put("role", "ROLE_EMPLOYEE");
+
+        // Issue post request
+        given().contentType("application/json").body(requestBody.toJSONString()).post(uri)
+                // Expected response
+                .then().statusCode(201);
+    }
+
+    @Test
+    public void saveUser_MissingParameter_ReturnBadRequest() throws Exception {
+        URI uri = new URI(baseUrl + port + baseEndpoint + "/register");
+
+        // Create request body in JSON -- missing name
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", "test420@gmail.com");
+        requestBody.put("password", "whatpassword");
+        requestBody.put("role", "ROLE_EMPLOYEE");
+
+        // Issue post request
+        given().contentType("application/json").body(requestBody.toJSONString()).post(uri)
+                // Expected response
+                .then().statusCode(400);
+    }
+
+    @Test
+    public void saveUser_BlankParameter_ReturnBadRequest() throws Exception {
+        URI uri = new URI(baseUrl + port + baseEndpoint + "/register");
+
+        // Create request body in JSON -- blank email
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", "");
+        requestBody.put("name", "Test");
+        requestBody.put("password", "whatpassword");
+        requestBody.put("role", "ROLE_EMPLOYEE");
+
+        // Issue post request
+        given().contentType("application/json").body(requestBody.toJSONString()).post(uri)
+                // Expected response
+                .then().statusCode(400);
+    }
+
+    @Test
+    public void saveUser_WhitespaceInput_ReturnBadRequest() throws Exception {
+        URI uri = new URI(baseUrl + port + baseEndpoint + "/register");
+
+        // Create request body in JSON -- password is whitespace
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", "test420@gmail.com");
+        requestBody.put("name", "Test");
+        requestBody.put("password", " ");
+        requestBody.put("role", "ROLE_EMPLOYEE");
+
+        // Issue post request
+        given().contentType("application/json").body(requestBody.toJSONString()).post(uri)
+                // Expected response
+                .then().statusCode(400);
+    }
+
+    @Test
+    public void saveUser_EmailIsNotAnEmail_ReturnBadRequest() throws Exception {
+        URI uri = new URI(baseUrl + port + baseEndpoint + "/register");
+
+        // Create request body in JSON -- email does not follow email format
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", "test");
+        requestBody.put("name", "Test");
+        requestBody.put("password", "password");
+        requestBody.put("role", "ROLE_EMPLOYEE");
+
+        // Issue post request
+        given().contentType("application/json").body(requestBody.toJSONString()).post(uri)
+                // Expected response
+                .then().statusCode(400);
+    }
+
+    // --------------------------------------------------
+    // Tests for logging in
+    // --------------------------------------------------
+
+    @Test
+    public void createAuthenticationToken_success() throws Exception {
+        // Arrange
+        URI uri = new URI(baseUrl + port + baseEndpoint + "/login");
+        // Create dummy user entity
+        User testUser = new User();
+        testUser.setID((long) 1);
+        testUser.setEmail("test420@gmail.com");
+        testUser.setName("Test");
+        testUser.setPassword(passwordEncoder.encode("password"));
+        testUser.setRole("ROLE_ADMIN");
+        // Save dummy user into database
+        users.save(testUser);
+
+        // Create request body in JSON
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", "test420@gmail.com");
+        requestBody.put("password", "password");
+
+        // Issue post request
+        given().contentType("application/json").body(requestBody.toJSONString()).post(uri)
+                // Expected response
+                .then().statusCode(200);
+    }
+}
