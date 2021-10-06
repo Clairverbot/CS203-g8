26d33e8426030b1c46362adf11eac22fae433eba
StanfordDC <stanforddchandra@gmail.com>
2021-10-06 13:41:29 +0800

    Update ARTTestResultRepository.java

--- src/main/java/com/G2T8/CS203WebApp/repository/ARTTestResultRepository.java
+++ src/main/java/com/G2T8/CS203WebApp/repository/ARTTestResultRepository.java
@@ -1,6 +1,7 @@
 package src.main.java.com.G2T8.CS203WebApp.repository;
 import com.G2T8.CS203WebApp.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ARTTestResultRepository extends JpaRepository<ARTTestResult, Long>{
