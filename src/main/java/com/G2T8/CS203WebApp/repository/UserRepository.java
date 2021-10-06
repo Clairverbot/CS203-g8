51eb42e618bbfaa205f743677bdaece163cc0b8c
StanfordDC <stanforddchandra@gmail.com>
2021-10-06 20:56:04 +0800

    Added dependencies

--- src/main/java/com/G2T8/CS203WebApp/repository/UserRepository.java
+++ src/main/java/com/G2T8/CS203WebApp/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.G2T8.CS203WebApp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.*;
+import org.springframework.data.repository.query.Param;
 
 import com.G2T8.CS203WebApp.model.*;
 
