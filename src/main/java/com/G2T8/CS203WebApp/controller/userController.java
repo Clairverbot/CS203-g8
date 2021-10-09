a7c416c39fb02c6e2a3288f804f71346bf5e4c38
shaysnutss <akshayan.2020@scis.smu.edu.sg>
2021-10-09 17:36:30 +0800

    fixed user controller and changed user service and user

--- src/main/java/com/G2T8/CS203WebApp/controller/userController.java
+++ src/main/java/com/G2T8/CS203WebApp/controller/userController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.annotation.Repeatable;
 import java.util.*;
 import javax.validation.Valid;
 
@@ -13,7 +15,7 @@ import com.G2T8.CS203WebApp.repository.*;
 import com.G2T8.CS203WebApp.service.UserService;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping(value = "/api/v1/user")
 public class userController{
 
     @Autowired
@@ -21,122 +23,143 @@ public class userController{
     @Autowired
     private UserRepository userRepo;
 
+    //done
     // get all users
     @GetMapping("/findAll")
     public List<User> findAllUsers() {
         if(userService.getAllUsers() != null){
             return userService.getAllUsers();
         }
-        // actually supp to throw user not found exception
         throw new UserNotFoundException();
         
     }
 
+    //done
     // get user by Email ( necessary as we are logging in with email)
-    @RequestMapping("/email/{email}")
-    public User findUserByEmail(String email) {
+    @RequestMapping(value = "/findUserByEmail/{email}", method = RequestMethod.GET )
+    public User findUserByEmail(@PathVariable(value = "email")String email) {
         if(userService.getUserByEmail(email) != null){
             return userService.getUserByEmail(email);
         }else{
-            // actually supp to throw user not found exception
             throw new UserNotFoundException(email);
             
         }
     
     }
 
+    //done
     // get user by ID
-    @RequestMapping("/ID/{ID}")
-    public User findUserByID(Long ID) {
+    @RequestMapping(value = "/findUserByID/{ID}", method = RequestMethod.GET)
+    public User findUserByID(@PathVariable(value = "ID")Long ID) {
         if (userService.getUser(ID) != null) {
             return userService.getUser(ID);
         } else {
-            // actually supp to throw user not found exception
             throw new UserNotFoundException(ID);
             
         }
     }
 
-    // change a persons vaccination status
-    @PutMapping("/updateVaccinationStatus/{ID}/{vaccinationstatus}")
-    public ResponseEntity<String> updateVaccinationStatus(@PathVariable("ID") Long ID, @PathVariable("vaccinationstatus") int vaccinationstatus) {
 
-        int result = userService.updateUserVaccinationStatus(ID, vaccinationstatus);
-        if (result != 20) {
-            if(result == 10){
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }else{
-                return new ResponseEntity<>(HttpStatus.OK);
-            }
+    // done
+    // updatesVaccinationStatus
+    @PutMapping("/updateVaccinationStatus/{id}")
+    public User updateVaccinationStatus(@PathVariable Long id, @Valid @RequestBody User userLatest){
+        Optional<User> userop = userRepo.findById(id); 
+        if(userop.isPresent()){
+            User userReal = userop.get();
+            userReal = userLatest; 
+            return userRepo.save(userReal);
             
+
         }
-        //// actually supp to throw user not found exception
-        throw new UserNotFoundException(ID);
+        throw new UserNotFoundException(id);
 
         
     }
 
-    // change user email
-    @PutMapping("/updateEmail/{ID}/{email}")
-    public ResponseEntity<String> updateEmail(@PathVariable("ID") Long ID,
-            @PathVariable("email") String email) {
-
-        User user = userService.updateUserEmail(ID, email);
-        if (user != null) {
-            return new ResponseEntity<>(HttpStatus.OK);
+    // done
+    // updates password
+    @RequestMapping(value = "/updatepassword/{id}", method = RequestMethod.PUT)
+    public User updatePassword(@PathVariable Long id, @Valid @RequestBody User userLatest){
+        Optional<User> userop = userRepo.findById(id);
+        if(userop.isPresent()){
+            User userReal = userop.get();
+            userReal = userLatest; 
+            return userRepo.save(userReal);
         }
-        // // actually supp to throw user not found exception
-        throw new UserNotFoundException(ID);
+        throw new UserNotFoundException(id); 
     }
 
-    //change user password
-
-
-
-    // change name of user
-    @PutMapping("/updateName/{ID}/{name}")
-    public ResponseEntity<String> updateName(@PathVariable("ID") Long ID,
-            @PathVariable("name") String name) {
-
-        User user = userService.updateUserEmail(ID, name);
-        if (user != null) {
-            return new ResponseEntity<>(HttpStatus.OK);
+    //done
+    // updates name
+    @RequestMapping(value = "/updatename/{id}", method = RequestMethod.PUT)
+    public User updateName(@PathVariable Long id, @Valid @RequestBody User userLatest){
+        Optional<User> userop = userRepo.findById(id);
+        if (userop.isPresent()) {
+            User userReal = userop.get();
+            userReal = userLatest;
+            return userRepo.save(userReal);
         }
-        // actually supp to throw user not found exception
-        throw new UserNotFoundException(ID);
+        throw new UserNotFoundException(id);
     }
 
-    //change role of user
-    @PutMapping("/updateRole/{ID}/{role}")
-    public ResponseEntity<String> updateRole(@PathVariable("ID") Long ID, @PathVariable("role") String role) {
-
-        int result = userService.updateUserRole(ID, role);
-        if (result == 1) {// done ok
-            return new ResponseEntity<>(HttpStatus.OK);
-        }else if(result == 10){// user input invalid role like officer
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    //done
+    //updates role
+    @RequestMapping(value = "/updaterole/{id}", method = RequestMethod.PUT)
+    public User updateRole(@PathVariable Long id, @Valid @RequestBody User userLatest){
+        Optional<User> userop = userRepo.findById(id);
+        if(userop.isPresent() && (userLatest.getRole().equals("ROLE_BASIC") || userLatest.getRole().equals("ROLE_ADMIN"))){
+            User userReal = userop.get();
+            userReal = userLatest;
+            return userRepo.save(userReal);
         }
-        // if result == 20 and user not there
-        // actually supp to throw user not found exception
-        throw new UserNotFoundException(ID);
+        throw new UserNotFoundException(id);
     }
 
-    //change managerid of a user
-    @PutMapping("/updateManagerID/{ID}/{managerID}")
-    public ResponseEntity<String> updateManagerID(@PathVariable("ID") Long ID, @PathVariable("managerID") Long managerID) {
-
-        int result = userService.updateUserManagerID(ID, managerID);
-        if (result != 20) {
-            if(result == 10){
-                // suppposed manager inputted is not a manager
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-            // if 1 is returned means user and manager exist and manager is manager
-            return new ResponseEntity<>(HttpStatus.OK);
+    //updates managerid
+    @RequestMapping(value = "/updatemanagerid/{id}", method = RequestMethod.PUT)
+    public User updateManagerId(@PathVariable Long id, @Valid @RequestBody User userLatest){
+        Optional<User> userop = userRepo.findById(id);
+        if(userop.isPresent()){
+            User userReal = userop.get();
+            userReal = userLatest;
+            return userRepo.save(userReal);
         }
-        // actually supp to throw user not found exception
-        throw new UserNotFoundException(ID);
+        throw new UserNotFoundException(id); 
     }
+
+
+
+    
+    // decided not to do this bc ur logging in w the email
+    // // change user email
+    // @PutMapping("/updateEmail/{ID}/{email}")
+    // public ResponseEntity<String> updateEmail(@PathVariable("ID") Long ID,
+    //         @PathVariable("email") String email) {
+
+    //     User user = userService.updateUserEmail(ID, email);
+    //     if (user != null) {
+    //         return new ResponseEntity<>(HttpStatus.OK);
+    //     }
+    //     throw new UserNotFoundException(ID);
+    // }
+
+
+    // //change managerid of a user
+    // @PutMapping("/updateManagerID/{ID}/{managerID}")
+    // public ResponseEntity<String> updateManagerID(@PathVariable("ID") Long ID, @PathVariable("managerID") Long managerID) {
+
+    //     int result = userService.updateUserManagerID(ID, managerID);
+    //     if (result != 20) {
+    //         if(result == 10){
+    //             // suppposed manager inputted is not a manager
+    //             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    //         }
+    //         // if 1 is returned means user and manager exist and manager is manager
+    //         return new ResponseEntity<>(HttpStatus.OK);
+    //     }
+    //     throw new UserNotFoundException(ID);
+    // }
 
 
 
--- src/main/java/com/G2T8/CS203WebApp/service/UserService.java
+++ src/main/java/com/G2T8/CS203WebApp/service/UserService.java
@@ -26,6 +26,7 @@ public class UserService {
     }
 
     public User getUserByEmail(String email){
+        
         Optional<User> b = userRepository.findByEmail(email);
         if (b.isPresent()) {
             User user = b.get();
@@ -49,43 +50,32 @@ public class UserService {
 
     }
 
-    public int updateUserVaccinationStatus(Long ID, int vaccstatus) {
-        Optional<User> b = userRepository.findById(ID);
-        if (b.isPresent()) {
-            User user = b.get();
-            if( vaccstatus == 1 || vaccstatus == 0 || vaccstatus == 2){
-                user.setVaccinationStatus(vaccstatus);
-                userRepository.save(user);
-                return vaccstatus;
-            }else{
-                return 10; //new vac status wrong
-            }
-        } else
-            return 20;//not present
+
+    public User updateUserVaccinationStatus(User user){
+        return userRepository.save(user);
 
     }
 
-    public User updateUserName(Long ID, String newName) {
-        Optional<User> b = userRepository.findById(ID);
-        if (b.isPresent()) {
-            User user = b.get();
-            user.setName(newName);
-            return userRepository.save(user);
-        } else
-            return null;
+    public User updateUserName(User user){
+        return userRepository.save(user);
 
     }
-
-    public User updateUserPassword(Long ID, String newName) {
-        Optional<User> b = userRepository.findById(ID);
-        if (b.isPresent()) {
-            User user = b.get();
-            user.setName(newName);
-            return userRepository.save(user);
-        } else
-            return null;
+    
+    public User updatepassword(User user) {
+        return userRepository.save(user);
 
     }
+    public User updaterole(User user){
+        return userRepository.save(user);
+    }
+
+    public User updatemanagerid(User user) {
+        return userRepository.save(user);
+    }
+
+
+
+
 
     // public User updateUserTeamID(Long ID, Long TeamID) {
     //     Optional<User> b = userRepository.findById(ID);
@@ -98,38 +88,21 @@ public class UserService {
 
     // }
 
-    public int updateUserRole(Long ID, String role) {
-        
-        if(role.equals("Admin") || role.equals("Basic")){
-            Optional<User> b = userRepository.findById(ID);
-            if (b.isPresent()) {
-                User user = b.get();
-                user.setRole(role);
-                userRepository.save(user);
-                return 1; // done perfect
-            }else{
-                return 20; // user not found
-            } 
-        }
-        return 10; // user input wrong role
-    }
 
-    public int updateUserManagerID(Long ID, Long managerID) {
-        Optional<User> b = userRepository.findById(ID);
-        Optional<User> managerop = userRepository.findById(managerID);
-        if (b.isPresent() && managerop.isPresent()) {
-            User user = b.get();
-            User manager = managerop.get();
-            if(manager.getRole().equals("Admin")){
-                user.setManagerUser(manager);
-                userRepository.save(user);
-                return 1;//done perfect
-            }else{
-                return 10; // bad request bc the supposed inputted manager isnt a manager
-            }
-        } else
-            return 20;//user or manager not found
-    }
-
-    
-}
+//     public int updateUserManagerID(Long ID, Long managerID) {
+//         Optional<User> b = userRepository.findById(ID);
+//         Optional<User> managerop = userRepository.findById(managerID);
+//         if (b.isPresent() && managerop.isPresent()) {
+//             User user = b.get();
+//             User manager = managerop.get();
+//             if(manager.getRole().equals("Admin")){
+//                 user.setManager_user(manager);
+//                 userRepository.save(user);
+//                 return 1;//done perfect
+//             }else{
+//                 return 10; // bad request bc the supposed inputted manager isnt a manager
+//             }
+//         } else
+//             return 20;//user or manager not found
+//     }
+ }
