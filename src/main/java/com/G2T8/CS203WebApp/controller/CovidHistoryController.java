4fd8789c3fd04f3edd81d181680c13272d7a2ab1
shaysnutss <akshayan.2020@scis.smu.edu.sg>
2021-10-15 17:16:59 +0800

    added exception handler for CovidHistory

---
+++ src/main/java/com/G2T8/CS203WebApp/controller/CovidHistoryController.java
@@ -0,0 +1,20 @@
+package com.G2T8.CS203WebApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import javax.validation.Valid;
+
+import com.G2T8.CS203WebApp.exception.*;
+import com.G2T8.CS203WebApp.model.*;
+import com.G2T8.CS203WebApp.repository.*;
+import com.G2T8.CS203WebApp.service.*; 
+
+@RestController
+public class CovidHistoryController {
+
+    
+    
+}
