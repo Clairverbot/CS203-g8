bf39f732e91124e06a1458b17025b91b8f4a911e
shaysnutss <akshayan.2020@scis.smu.edu.sg>
2021-10-19 01:22:19 +0800

    news

---
+++ src/main/java/com/G2T8/CS203WebApp/controller/CMSController.java
@@ -0,0 +1,67 @@
+package com.G2T8.CS203WebApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.net.URL;
+import java.io.File;
+import java.util.*; 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+@RestController
+@RequestMapping("/api/v1/CMS")
+public class CMSController {
+
+    @GetMapping("/news")
+    public List<String> webParsing(){
+
+        ArrayList<String> overallPoints = new ArrayList<>();
+        try {
+            Document d = Jsoup.connect("https://www.mom.gov.sg/covid-19/requirements-for-safe-management-measures")
+                    .timeout(6000).get();
+            overallPoints.add("one");
+            Element block = d.select("body").select("#mainform").select("#MainContent").first();
+            Elements round2 = block.getElementsByClass("content-wrapper").first().getElementsByClass("container").first().getElementsByClass("page-content")
+                    .select("#pagecontent_0_documentcontent_0_DivCode").select("ol").first().getElementsByTag("li");
+             
+            overallPoints.add("four");
+            overallPoints.add("two");
+            
+            // loop for point 5 for example
+            for (Element bigelm : round2) {
+                String pointBlockAndChildren = bigelm.text();
+                overallPoints.add(pointBlockAndChildren);
+                overallPoints.add("three");
+            }
+
+        } catch (Exception e) {
+
+        }
+        overallPoints.add("tst");
+        return overallPoints; 
+
+    }
+
+    
+
+}
+
+
+
+
+
+
+
+        
+    
+        
+
+    
+
+
+    
+
