bc51f095b891ed60cb614bbbeaa6a08a75d5f648
shelle-mim <michelleleong8@gmail.com>
2021-10-20 14:58:26 +0800

    resolve conflicts

--- src/main/java/com/G2T8/CS203WebApp/model/Temperature.java
+++ src/main/java/com/G2T8/CS203WebApp/model/Temperature.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.G2T8.CS203WebApp.model;
 
 import javax.persistence.*;
@@ -20,26 +19,3 @@ public class Temperature {
     @JsonBackReference
     private User user;
 }
-=======
-package com.G2T8.CS203WebApp.model;
-
-import javax.persistence.*;
-import lombok.*;
-import java.time.LocalDateTime;
-import com.fasterxml.jackson.annotation.*;
-
-@Data
-@NoArgsConstructor
-@Entity
-public class Temperature {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long tempId;
-    private LocalDateTime date;
-    private double temperature;
-    @ManyToOne
-    @JoinColumn(name="user_id")
-    @JsonBackReference
-    private User user;
-}
->>>>>>> 3ed83d92a7624461766920a6ec8416c07b046ccd
--- src/test/java/com/G2T8/CS203WebApp/Cs203WebAppApplicationTests.java
+++ src/test/java/com/G2T8/CS203WebApp/Cs203WebAppApplicationTests.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.G2T8.CS203WebApp;
 
 import org.junit.jupiter.api.Test;
@@ -12,18 +11,3 @@ class Cs203WebAppApplicationTests {
 	}
 
 }
-=======
-package com.G2T8.CS203WebApp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Cs203WebAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
->>>>>>> 3ed83d92a7624461766920a6ec8416c07b046ccd
